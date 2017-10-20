ceeac967172b65b4518203a48e14bfb72ce235b9
puppy955101 <puppy955101@gmail.com>
2017-10-20 10:47:17 +0800

    can enter menu from add_mode

--- app/src/main/java/puppy/timemanagement/add_something.java
+++ app/src/main/java/puppy/timemanagement/add_something.java
@@ -1,16 +1,35 @@
 package puppy.timemanagement;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 /**
  * Created by charles on 2017/10/18.
  */
 public class add_something extends ActionBarActivity {
+    EditText et;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_mode);
+        et = (EditText) findViewById(R.id.editText);
+        Button add = (Button) findViewById(R.id.button1);
+        add.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View arg0) {
+                SharedPreferences SP = getSharedPreferences("data", MODE_PRIVATE);
+                SP.edit().putString("add", et.getText().toString()).apply();
+                Intent intent = new Intent();
+                intent.setClass(add_something.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
